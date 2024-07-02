70bfadc88e28f8628620a5707786ae933e586400
agon <agon@giskos.com>
2024-07-02 13:50:58 +0200

    tipet e te dhenave

---
+++ 4_tptet_e_te_dhenave.py
@@ -0,0 +1,41 @@
+#Tipet e te dhënave
+
+#Në Python, tipet e të dhënave janë ndarë në dy kategori:
+#1. Tipet e te dhenave baze (primitive data types)
+#2. Tipet e te dhenave te avancuara (advanced data types)
+
+#Tipet e te dhenave baze
+#int - Numra te plote
+x = 5
+print(type(x))
+
+#float - Numra me presje
+x = 20.5
+print(type(x))
+
+#complex - Numra kompleks
+x = 1j
+print(type(x))
+
+#str - String
+x = "Pershendetje"
+print(type(x))
+
+
+#Tipet e te dhenave te avancuara
+
+#list - Liste
+x = ["e gjelber", "e kuqe", "e verdhe"]
+print(type(x))
+
+#tuple - Tuple
+x = ("e gjelber", "e kuqe", "e verdhe")
+print(type(x))
+
+#set - Set
+x = {"e gjelber", "e kuqe", "e verdhe"}
+print(type(x))
+
+#dict - Dictionary
+x = {"marka": "Ford", "modeli": "Mustang", "viti": 1964}
+print(type(x))
